9eaa9aecdbdb315a66ee2e4966464f518dd05c2e
aequis <70807250+aequis@users.noreply.github.com>
2021-12-19 17:53:09 +0100

    Add monsters

--- src/App.js
+++ src/App.js
@@ -7,19 +7,30 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello"
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: 'afie',
+        },
+        {
+          name: "Dracula",
+          id: 'fiej',
+        },
+        {
+          name: "Zombie",
+          id: 'nieo'
+        }
+      ]
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello Andrei" })}>
-            Change text
-          </button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>)
+          )
+        }
       </div>
     )
   }
